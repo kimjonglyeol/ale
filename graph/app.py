b882331a91a41896225837e260c9fa21dbe4deca
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-13 15:30:45 +0900

    a

--- graph/app.py
+++ graph/app.py
@@ -28,5 +28,6 @@ st.pyplot(fig)
 
 x = [10, 60, 30] # 범주형 데이터별 파이 그래프의 비율
 labels = ['A', 'B', 'C']
-
+fig = plt.figure(figsize=(8, 4))
 plt.pie(x=x, labels=labels, autopct='%.1f%%')
+st.pyplot(fig)
