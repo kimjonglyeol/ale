6fac3efde983bafa4a31f2b144b5a3523bdfff35
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-13 17:53:08 +0900

    a

--- cafe/app.py
+++ cafe/app.py
@@ -4,9 +4,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 st.write(
    "https://www.data.go.kr/data/15081069/fileData.do"
 )
 df = pd.read_csv('./cafe/cafe.csv', encoding='CP949')
 st.write(df)
+
+from glob import glob
+
+file_names = glob('./cafe/*.csv')
+total = pd.DataFrame()
+
+for file_name in file_names:
+   temp = pd.read.csv(file_name, sep='|', encoding='utf=--8')
+   total = pd.concat([total, temp])
+
+ total.reset_index(inplace=True, drop=True)
+ total  
