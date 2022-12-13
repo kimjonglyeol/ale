10096d79fcf0325f8b55b9ce332ab95be887ffe9
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-13 11:45:23 +0900

    추가 수정

---
+++ first_webapp/app.py
@@ -0,0 +1,16 @@
+# streamlit 라이브러리 호출
+import streamlit as st
+
+
+st.write(
+    """
+    # 제 첫 웹페이지닙니다.
+    ## 부족하지만
+    * 원소주 땡긴다.
+    """
+)    
+
+st.image(
+
+    "https://pixabay.com/photos/soju-shot-glass-drink-glass-6523540/"
+)
