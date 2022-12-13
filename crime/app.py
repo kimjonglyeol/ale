c2e0b64557d604b143ffe9ff05a9ea0078aa507d
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-13 17:00:07 +0900

    ㅁ

--- crime/app.py
+++ crime/app.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import matplotlib.pyplot as plt
 plt.rc('font', family='NanumBarunGothic') 
 
 st.write(
