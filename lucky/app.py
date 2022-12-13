b472774a2a09320a5b92a10e7fbdf3ca32ffc2e5
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-13 14:16:15 +0900

    a

--- lucky/app.py
+++ lucky/app.py
@@ -10,11 +10,12 @@ st.header("여러분의 참여를 환영합니다")
 # 열을 배치하는 메소드
 columns = st.columns(4)    # 화면을 열로 나누어서 배치
 # 가로 4개의 열
+# enumerate : index, value 묶
 for idx, col in enumerate(columns):
     for idx2 in range(4):
         # 키가 겹치면 안됨
         col.text_input(f"조 추첨 대상 {idx + 1 + idx2 * 4}", key=f"{idx + 1 + idx2 * 4}")
 # 13명이 소속될 조 이름을 넣을 위치
-
+st.write(st.session_state)
 # <추첨 버튼>
 # 13개의 짝을 지어서 표시해줄 그래픽
