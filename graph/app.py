9f444031976f49401cf748215fecb0c92d4cef95
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-13 15:22:10 +0900

    a

--- graph/app.py
+++ graph/app.py
@@ -5,8 +5,16 @@ import pandas as pd
 import matplotlib as plt
 import seaborn as sns
 
-st.write('hello')
+st.write(
+    """
+    ## 데이터의 종류
+    * 데이터의 특성에 따라서 시각화 방식도 달라짐
+    * 정형 데이터 : 값으로 나타낼 수 있는 데이터 (숫자)
+    * 비정형 데이터 : 정형 데이터가 아닌 것 (사진, 언어...)
+    """
+)
 
 titanic = sns.load_dataset('titanic') 
-# st.write(titanic)
-st.table(titanic)
+st.write(titanic)
+# st.table(titanic)
+
