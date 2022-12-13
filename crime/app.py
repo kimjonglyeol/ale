c21b7c5081645e0b8534a95887a7f2e384941a8b
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-13 16:56:30 +0900

    ㅁ

--- crime/app.py
+++ crime/app.py
@@ -3,8 +3,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 st.write(
    "https://www.data.go.kr/data/15084748/fileData.do"
 )
 df = pd.read_csv('./crime/crime.csv', encoding='CP949')
 st.write(df)
+
+fig = plt.figure(figsize=(10,4))
+sns.histplot(data=df, x='발생건수(건)')
+st.pyplot(fig)
