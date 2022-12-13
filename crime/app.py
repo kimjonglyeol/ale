699204410cd684b2cbcb63e70f92e3ffdc8f2bcc
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-13 16:22:24 +0900

    a

--- crime/app.py
+++ crime/app.py
@@ -6,5 +6,5 @@ import seaborn as sns
 st.write(
    "https://www.data.go.kr/data/15084748/fileData.do"
 )
-df = pd.read_csv('./crime/crime.csv')
+df = pd.read_csv('./crime/crime.csv', encoding='CP949')
 st.write(df)
