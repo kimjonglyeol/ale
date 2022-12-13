5f2976b1d0b110e7eb609a85c1f5cc156f336782
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-13 14:21:13 +0900

    a

--- lucky/app.py
+++ lucky/app.py
@@ -8,9 +8,9 @@ st.header("여러분의 참여를 환영합니다")
 # 추첨 대상인 13명의 이름을 넣을 수 있는 text.input
 # 3 x 4 (row, col)
 # 열을 배치하는 메소드
-tabs = st.tabs(['참가자'])
-
-columns = st.columns(4)    # 화면을 열로 나누어서 배치
+tabs = st.tabs(['참가자','조'])
+# 0번째 탭에 컬럼(열)을 넣겠다
+columns = tabs[0].columns(4)    # 화면을 열로 나누어서 배치
 # 가로 4개의 열
 # enumerate : index, value 묶
 for idx, col in enumerate(columns):
