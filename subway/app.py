8b882043756f221f5a3f0efa7948ea8c42487621
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-14 12:26:05 +0900

    a

--- subway/app.py
+++ subway/app.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
-
-st.write(
-   "https://www.naver.com/"
+st.title("지하철")
+# st.write(
+#    "https://www.naver.com/"
 )
 df = pd.read_csv('./subway/subway.csv', encoding='CP949')
 st.write(df)
 
-df2 = pd.read_csv('./subway/subway_part.csv')
-st.write(df2)
+# df2 = pd.read_csv('./subway/subway_part.csv')
+# st.write(df2)
 
 fig = plt.figure(figsize=(10,4))
 sns.histplot(data=df, x='호선', hue='조사일자', multiple='stack')
