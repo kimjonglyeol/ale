0fb5637f84760c05965c50cfdd31ae25d6d53848
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-14 12:37:39 +0900

    ㅁ

--- subway/app.py
+++ subway/app.py
@@ -20,28 +20,28 @@ st.write(df)
 # df2 = pd.read_csv('./subway/subway_part.csv')
 # st.write(df2)
 
-fig = plt.figure(figsize=(10,4))
-sns.histplot(data=df, x='호선', hue='조사일자', multiple='stack')
-st.pyplot(fig)
+# fig = plt.figure(figsize=(10,4))
+# sns.histplot(data=df, x='호선', hue='조사일자', multiple='stack')
+# st.pyplot(fig)
 
-fig2 = plt.figure(figsize=(10,4))
-sns.kdeplot(data=df, x='호선')
-sns.rugplot(data=df, x='호선')
-st.pyplot(fig2)
+# fig2 = plt.figure(figsize=(10,4))
+# sns.kdeplot(data=df, x='호선')
+# sns.rugplot(data=df, x='호선')
+# st.pyplot(fig2)
 
-fig3 = plt.figure(figsize=(10,4))
-sns.kdeplot(data=df, x='호선', hue='조사일자', multiple='stack')
-st.pyplot(fig3)
+# fig3 = plt.figure(figsize=(10,4))
+# sns.kdeplot(data=df, x='호선', hue='조사일자', multiple='stack')
+# st.pyplot(fig3)
 
-fig4 = plt.figure(figsize=(10,4))
-sns.displot(data=df, x='호선')
-st.pyplot(fig4)
+# fig4 = plt.figure(figsize=(10,4))
+# sns.displot(data=df, x='호선')
+# st.pyplot(fig4)
 
-x = [10, 60, 30] # 범주형 데이터별 파이 그래프의 비율
-labels = ['A', 'B', 'C']
-fig5 = plt.figure(figsize=(10, 4))
-plt.pie(x=x, labels=labels, autopct='%.1f%%')
-st.pyplot(fig5)
+# x = [10, 60, 30] # 범주형 데이터별 파이 그래프의 비율
+# labels = ['A', 'B', 'C']
+# fig5 = plt.figure(figsize=(10, 4))
+# plt.pie(x=x, labels=labels, autopct='%.1f%%')
+# st.pyplot(fig5)
 
 fig = px.histogram(df, x='호선',title='호선별 이용자수' )
 st.plotly_chart(fig)
