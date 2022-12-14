4df0efa11c4c23bae36c7ab6624fe6ec9bc440b8
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-14 14:13:29 +0900

    a

--- subway/app.py
+++ subway/app.py
@@ -43,7 +43,8 @@ st.write(df)
 # plt.pie(x=x, labels=labels, autopct='%.1f%%')
 # st.pyplot(fig5)
 
-tabs = st.tabs(['호선별 이용자수'])
+tabs = st.tabs(['호선별 이용자수','5시30분 이용자수'])
+columns = tabs[0]
 fig = px.histogram(df, x='호선')
 st.plotly_chart(fig)
 
@@ -51,7 +52,7 @@ df.pivot_table(index='호선', columns='구분', values='5시30분', aggfunc='su
 fp = df.pivot_table(index='호선', columns='구분', values='5시30분', aggfunc='sum')
 zp = fp.fillna(0)
 # zp
-
+columns2 = tabs[1]
 # df7 = px.data.df()
 fig7 = px.density_heatmap(df, x='호선', y="5시30분", marginal_x="rug", marginal_y="histogram")
 st.plotly_chart(fig7)
