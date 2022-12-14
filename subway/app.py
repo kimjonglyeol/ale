84aefe516de5d4e60896dbf130fe762b31027674
kimjonglyeol <120434446+kimjonglyeol@users.noreply.github.com>
2022-12-14 12:12:52 +0900

    a

--- subway/app.py
+++ subway/app.py
@@ -48,7 +48,7 @@ zp
 
 sns.heatmap(data=zp)
 
-df7 = px.data.df()
-fig7 = px.density_heatmap(data = df7, x="구분", y="호선", marginal_x="rug", marginal_y="histogram")
+# df7 = px.data.df()
+fig7 = px.density_heatmap(data = zp, x="구분", y="호선", marginal_x="rug", marginal_y="histogram")
 st.plotly_chart(fig7)
 
